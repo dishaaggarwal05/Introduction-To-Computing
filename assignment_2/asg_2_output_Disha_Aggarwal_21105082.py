0fbf9dcb90988bbd911a5670bcfbdea53d4f45dd
dishaaggarwal05 <97087823+dishaaggarwal05@users.noreply.github.com>
2022-01-21 22:36:51 +0530

    Add files via upload

---
+++ assignment_2/asg_2_output_Disha_Aggarwal_21105082.py
@@ -0,0 +1,50 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: D:\PEC STUDY MATERIAL_SEM_1\ESC1\ASSIGNMENT\input\asg_2_input_Disha_Aggarwal_21105082.py
+
+                       #question_1
+(a) length of string : 35
+(b) reversed string : egaugnal evitisnes esac a si nohtyP
+(c) new string : a case sensitive
+(d) after replacing : Python is object oriented language
+(e) index of a : 10
+(f) after removing white spaces : Pythonisacasesensitivelanguage
+
+                       #question_2
+what is your name : Disha Aggarwal
+your SID : 21105082
+department name : ECE
+your CGPA : 9.6
+
+Hey, Disha Aggarwal Here!
+My SID is 21105082 
+I am from ECE department and my CGPA is 9.600000
+
+                      #question_3
+(a) a&b : 8
+(b) a|b : 58
+(c) a^b : 50
+(d) Left shift a with 2 bits : 224
+    Left shift b with 2 bits : 40
+(e) Right shift a with 2 bits : 14
+    Right shift b with 4 bits : 0
+
+                      #question_4
+enter first number : 22
+enter second number : 7
+enter third number : 98
+
+the greatest no is : 98
+
+                      #question_5
+enter string : my name is Disha
+
+yes
+
+                      #question_6
+enter length of first side : 5
+enter length of second side : 7
+enter length of third side : 6
+
+yes
