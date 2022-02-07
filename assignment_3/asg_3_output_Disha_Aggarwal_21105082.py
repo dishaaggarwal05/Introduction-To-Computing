5c7c1f722739e013e071425a1f45c39a27b1176a
dishaaggarwal05 <97087823+dishaaggarwal05@users.noreply.github.com>
2022-02-07 11:37:18 +0530

    Add files via upload

---
+++ assignment_3/asg_3_output_Disha_Aggarwal_21105082.py
@@ -0,0 +1,86 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: D:\PEC STUDY MATERIAL_SEM_1\ESC1\ASSIGNMENT\input\asg_3_input_Disha_Aggarwal_21105082.py
+
+                        #question_1
+enter the string: aggarwal
+aggarwal
+{'a': 3, 'g': 2, 'r': 1, 'w': 1, 'l': 1}
+
+                          #question_2
+enter Day: 05
+enter Month: 11
+enter Year: 2003
+Next Date is: 6/11/2003
+
+                      #question_3
+Enter the numbers separated by space:1 2 3 4 5 6 7 8
+
+List containing (n,n^2) is :
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64)]
+
+                     #question_4
+Enter your grade points: 9
+Your grade is 'A' and Excellent performance.
+
+                     #question_5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+
+                      #question 6
+Enter SID of Student: 21105082
+Enter the name of the Student: Disha Aggarwal
+If you want to enter more details press Y/N: Y
+Enter SID of Student: 21105026
+Enter the name of the Student: Muskaan Aneja
+If you want to enter more details press Y/N: Y
+Enter SID of Student: 21105084
+Enter the name of the Student: Nakul Garg
+If you want to enter more details press Y/N: Y
+Enter SID of Student: 21105101
+Enter the name of the Student: Aditya Singla
+If you want to enter more details press Y/N: N
+(a)
+21105082 is Disha Aggarwal
+21105026 is Muskaan Aneja
+21105084 is Nakul Garg
+21105101 is Aditya Singla
+
+(b)
+value sorted dictionary is ['Aditya Singla', 'Disha Aggarwal', 'Muskaan Aneja', 'Nakul Garg']
+
+(c)
+Keys sorted dictionary is [21105026, 21105082, 21105084, 21105101]
+
+(d)
+e\Enter SID whose detail you need- 21105082
+Name of the required student is Disha Aggarwal
+
+
+                      #question_7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 5
+Fibonnaci sequence with 5 terms
+0
+1
+1
+2
+3
+Sum of total 5 terms of sequence is 7
+END
+
+                   #question_8
+(a)
+set with elements not common in both is {1, 3, 5, 6, 8}
+(b)
+Set of elements that is only present in one set is {17, 3, 6, 8, 9, 13}
+(c)
+Set of elements that is only present in two set is {1, 2, 4, 5}
+(d)
+set of all integers in the range 1 to 10 that are not in Set_1 {6, 7, 8, 9, 10}
+(e)
+set of all integers in the range 1 to 10 that are not in Set_1 and Set_2 and Set_3 {10, 7}
